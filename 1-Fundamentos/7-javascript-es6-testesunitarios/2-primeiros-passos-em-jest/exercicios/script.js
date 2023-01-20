d99f9979a54d5d98593273c76f422beea5dae801
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-01-20 12:27:16 -0300

    Feat: Resolve todos os exercícios do dia - Primeiros passos em Jest

--- 1-Fundamentos/7-javascript-es6-testesunitarios/2-primeiros-passos-em-jest/exercicios/script.js
+++ 1-Fundamentos/7-javascript-es6-testesunitarios/2-primeiros-passos-em-jest/exercicios/script.js
@@ -18,4 +18,86 @@ function myRemove(arr, item) {
   return newArr;
 }
 
-module.exports = myRemove;
+// A função myFizzBuzz(num) recebe um número num como parâmetro. Caso num seja um número divisível por 3 e 5, a função retorna "fizzbuzz". Caso num seja um número divisível apenas por 3, retorna "fizz". E caso num seja um número divisível apenas por 5, retorna "buzz". Se num for um número que não é divisível nem por 3 e nem por 5, a função retorna o próprio número num. Caso num não seja um número, a função retorna false.
+// Execute a função myFizzBuzz(num), sendo num um número divisível por 3 e 5, e verifique se o retorno é o esperado.
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+const mapString = (objectMap, string) => {
+  const splitString = string.split('');
+  const mappedArray = [];
+  for (let index = 0; index < splitString.length; index += 1) {
+    const character = splitString[index];
+    const mappedValue = objectMap[character];
+    
+    if (mappedValue) {
+      mappedArray.push(mappedValue);
+    } else {
+      mappedArray.push(character);
+    }
+  }
+  return mappedArray.join('');
+}
+const encode = (string) => {
+  const map = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  return mapString(map, string);
+}
+const decode = (string) => {
+  const map = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  return mapString(map, string);
+}
+
+const techList = (arr, name) => {
+  const newArr = [];
+  arr.sort();
+  if (arr.length === 0) {
+    return 'Vazio!'
+  }
+  for (let index = 0; index < arr.length; index += 1) {
+    newArr.push({
+      tech: arr[index],
+      name: name,
+    });
+  }
+  return newArr;
+}
+
+// A função hydrate recebe uma string no formato “numero bebida”, e retorna a sugestão de quantos copos de água você deve beber para se hidratar. Para cada bebida, deve-se tomar um copo de água para não ter ressaca. Exemplo:
+
+const hydrate = (string) => {
+  let contador = 0;
+  for (let index = 0; index < string.length; index += 1) {
+    let sugestao = Number(string[index])
+    if (sugestao) {
+      contador += sugestao
+    }
+  }
+  if (contador === 1) {
+    return `${contador} copo de água`
+  }
+  return `${contador} copos de água`
+}
+
+console.log(hydrate('5 cachaças, 5 cachaças, 5 cachaças, 5 cachaças, 5 cachaças'));
+
+// implemente seus testes aqui
+
+module.exports = { myRemove, myFizzBuzz, mapString, encode, decode, techList, hydrate}
--- 1-Fundamentos/7-javascript-es6-testesunitarios/2-primeiros-passos-em-jest/exercicios/script.test.js
+++ 1-Fundamentos/7-javascript-es6-testesunitarios/2-primeiros-passos-em-jest/exercicios/script.test.js
@@ -1,4 +1,4 @@
-const myRemove = require('./script.js');
+const {myRemove, myFizzBuzz, mapString, encode, decode, techList, hydrate} = require('./script.js');
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado;
 
@@ -11,5 +11,86 @@ describe('Testar a função myRemove', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
+
+describe('Testar a função myFizzBuzz', () => {
+  it('Verificar se o retorno é o esperado', () => {
+    expect(myFizzBuzz(15)).toEqual('fizzbuzz');
+    expect(myFizzBuzz(21)).toEqual('fizz');
+    expect(myFizzBuzz(20)).toEqual('buzz');
+    expect(myFizzBuzz(2)).toEqual(2);
+    expect(myFizzBuzz('oi')).toEqual(false);
+  });
+});
+
+describe('Testa a função encode e decode', () => {
+  it('Testar se encode e decode são functions', () => {
+    expect(typeof encode).toEqual('function');
+    expect(typeof decode).toEqual('function');
+  });
+  it('Teste se os números são convertidos em vogais', () => {
+    expect(encode('a, e, i, o, u')).toEqual('1, 2, 3, 4, 5');
+  });
+  it('Teste se os números são convertidos em vogais', () => {
+    expect(decode('1, 2, 3, 4, 5')).toEqual('a, e, i, o, u');
+  });
+  it('Teste se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro.', () => {
+    expect(encode('trybe')).toHaveLength(5);
+    expect(decode('trybe')).toHaveLength(5);
+  });
+});
+
+// Implemente a função techList a partir dos testes abaixo. É importante nunca alterar os testes ou as variáveis já escritas no código.
+
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
+  });
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
+  });
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas'
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas'
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas'
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas'
+      },
+      {
+        tech: 'React',
+        name: 'Lucas'
+      }
+    ]);
+  });
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Lucas')).toBe('Vazio!');
+  });
+});
+
+describe('Testa a função hydrate', () => {
+  it('Testa se a função hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
+    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
   });
 });
